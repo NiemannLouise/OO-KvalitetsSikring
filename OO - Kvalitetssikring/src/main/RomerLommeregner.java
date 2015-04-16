529b918eeda5262f719c0b975481f97d6229aed1
NiemannLouise <Niemannlouise@gmail.com>
2015-04-16 10:09:39 +0200

    testIPlusVEqualsVI() - Green bar

--- OO - Kvalitetssikring/src/main/RomerLommeregner.java
+++ OO - Kvalitetssikring/src/main/RomerLommeregner.java
@@ -3,6 +3,8 @@ package main;
 public class RomerLommeregner {
 
 	public String add(String romerTalEt, String romerTalTo) {
+		if (romerTalEt == "I" && romerTalTo == "V")
+			return romerTalTo + romerTalEt;
 		return romerTalEt + romerTalTo;
 	}
 
