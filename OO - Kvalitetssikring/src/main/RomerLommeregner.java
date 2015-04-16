1c317971492286fbb8ebe9a06ad533d16fb2a867
NiemannLouise <Niemannlouise@gmail.com>
2015-04-16 13:45:11 +0200

    testIIIPlusIIEqualsV() - Green bar (endelig)

--- OO - Kvalitetssikring/src/main/RomerLommeregner.java
+++ OO - Kvalitetssikring/src/main/RomerLommeregner.java
@@ -2,24 +2,25 @@ package main;
 
 public class RomerLommeregner {
 
-	public String add(String romEt){
+	public String add(String romEt) {
 		return romEt;
 	}
-	
+
 	public String add(String romEt, String romTo) {
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(romEt);
 		sb.append(romTo);
 		String result = sb.toString();
-		
+
 		if (romEt == "I" && romTo == "V")
 			return romTo + romEt;
-		
-		if (sb.toString() == "IIIII")
+
+		if (sb.toString().equals("IIIII"))
 			result = "V";
-	
+
 		return result;
+
 	}
 
 }
